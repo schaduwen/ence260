882b8a6efabfeae0da3a5ee2adc6d64a83235d71
Richard Clare <richard.clare@canterbury.ac.nz>
2017-11-08 13:02:03 +1300

    revert timer.c back to unfilled

--- labs/lab2-ex2/timer.c
+++ labs/lab2-ex2/timer.c
@@ -5,9 +5,6 @@
 void timer_init (void)
 {
     /* TODO */
-    TCCR1A = 0x00;
-    TCCR1B = 0x05;
-    TCCR1C = 0x00;
 
 }
 
@@ -15,19 +12,11 @@ void timer_init (void)
 /* Wait for the specified length of time.  */
 void timer_delay_ms (uint16_t milliseconds)
 {
-    uint16_t count_stop=0;
 
     /* TODO: Calculate the timer/counter value needed 
        for the given number of milliseconds. */
-    count_stop=milliseconds*milliseconds*(8e6/1024)/1000;
 
     /* TODO: Wait for the timer/couter to reach the 
        value calculated above.  */
-    TCNT1=0;  /* reset the counter */
-
-    while (TCNT1 < count_stop)
-    {
-        continue;
-    }
 
 }
